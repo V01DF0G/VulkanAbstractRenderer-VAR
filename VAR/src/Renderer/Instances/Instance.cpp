8bb3179ac0f382c2549fc9bb3cd5e48e09c1babf
V01DF0G <V01DF0G@users.noreply.github.com>
2020-12-18 00:29:37 +0200

    Init

--- VAR/src/Renderer/Instances/Instance.cpp
+++ VAR/src/Renderer/Instances/Instance.cpp
@@ -5,34 +5,13 @@
 
 Instance::Instance()
 {
-	if (enableValidationLayers && !checkValidationLayerSupport())
-	{
-		RENDER_LOG_CRIT("Validation Layers requested but there is no support for them");
-	}
 	std::unique_ptr<VkInstanceCreateInfo> InstanceCreateInfo(new VkInstanceCreateInfo());
 	InstanceCreateInfo->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	InstanceCreateInfo->pApplicationInfo = m_AppInfo.m_VKAppInfo.get();
 
-	auto extensions = getRequiredExtensions();
-	InstanceCreateInfo->enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-	InstanceCreateInfo->ppEnabledExtensionNames = extensions.data();
+	InstanceCreateInfo->enabledLayerCount = 0;
 
-	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
-
-	if (enableValidationLayers == true && checkValidationLayerSupport())
-	{
-		InstanceCreateInfo->enabledLayerCount = static_cast<uint32_t>(ValLayer.size());
-		InstanceCreateInfo->ppEnabledLayerNames = ValLayer.data();
-		
-		populateDebugMessengerCreateInfo(debugCreateInfo);
-		InstanceCreateInfo->pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
-	}
-	else
-	{
-		InstanceCreateInfo->enabledLayerCount = 0;
-
-		InstanceCreateInfo->pNext = nullptr;
-	}
+	InstanceCreateInfo->pNext = nullptr;
 
 	if (vkCreateInstance(InstanceCreateInfo.get(), nullptr, &m_VkInstance) != VK_SUCCESS)
 	{
@@ -42,57 +21,11 @@ Instance::Instance()
 	{
 		RENDER_LOG_INFO("Successfully created the Instance !");
 	}
-	if (enableValidationLayers)
-	{
-		setupDebugMessenger();
-	}
 }
 
 Instance::~Instance()
 {
-	if (enableValidationLayers);
-	{
-		DestroyDebugUtilsMessengerEXT(m_VkInstance, debugMessenger, nullptr);
-	}
 	vkDestroyInstance(m_VkInstance, nullptr);
-
 }
 
-VKAPI_ATTR VkBool32 VKAPI_CALL Instance::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageseverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData)
-{
-	switch (messageseverity)
-	{
-	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
-		RENDER_LOG_INFO("{0}", pCallbackData->pMessage);
-		return VK_FALSE;
-	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
-		RENDER_LOG_TRACE("{0}", pCallbackData->pMessage);
-		return VK_FALSE;
-	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
-		RENDER_LOG_WARN("{0}", pCallbackData->pMessage);
-		return VK_FALSE;
-	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
-		RENDER_LOG_ERR("{0}", pCallbackData->pMessage);
-		return VK_TRUE;
-	default:
-		return VK_FALSE;
-	}
-}
-
-void Instance::setupDebugMessenger()
-{
-	if (!enableValidationLayers)
-	{
-		return;
-	}
-
-	VkDebugUtilsMessengerCreateInfoEXT createInfo;
-	populateDebugMessengerCreateInfo(createInfo);
-
-	if (CreateDebugUtilsMessengerEXT(m_VkInstance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
-	{
-		RENDER_LOG_ERR("failed to set up debug messenger!");
-		exit(1);
-	}
-}
 
